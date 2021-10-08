13fc6230748265d4b5d6bbc49e72deff730e7a9c
Sam Martin Vargas Giagnocavo <samfefuty@hotmail.com>
2021-10-08 19:11:57 +0200

    For some reason window.location.port is a string?

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   // Set the URL for the API.
   api_url: `://${window.location.hostname}:${window.location.port}/api/`,
   mqtt_host: window.location.hostname,
-  mqtt_port: window.location.port
+  mqtt_port: parseInt(window.location.port) || 8080
 };
