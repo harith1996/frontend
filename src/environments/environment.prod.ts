ab10058cc492635e0439f6129d6a309764621bca
Sam Martin Vargas Giagnocavo <2337-au703393@users.noreply.gitlab.au.dk>
2021-10-08 16:55:11 +0000

    We proxy the web socket connection so mqtt_port isn't different from the website port.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,6 @@ export const environment = {
   rest_proto: "http",
   // Set the URL for the API.
   api_url: `://${window.location.hostname}:${window.location.port}/api/`,
-  //mqtt_host: "broker",
   mqtt_host: window.location.hostname,
-  //mqtt_port: 8883
-  mqtt_port: 8080
+  mqtt_port: window.location.port
 };
